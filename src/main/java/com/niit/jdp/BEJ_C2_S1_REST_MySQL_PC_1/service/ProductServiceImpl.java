edeb4d14fa2a813215a6dbfa9a6a824885dad072
Praveen Kumar <praveen170596@gmail.com>
2022-11-22 12:23:11 +0530

    created constructor for the ProductRepository class

--- src/main/java/com/niit/jdp/BEJ_C2_S1_REST_MySQL_PC_1/service/ProductServiceImpl.java
+++ src/main/java/com/niit/jdp/BEJ_C2_S1_REST_MySQL_PC_1/service/ProductServiceImpl.java
@@ -8,10 +8,17 @@
 package com.niit.jdp.BEJ_C2_S1_REST_MySQL_PC_1.service;
 
 import com.niit.jdp.BEJ_C2_S1_REST_MySQL_PC_1.domain.Product;
+import com.niit.jdp.BEJ_C2_S1_REST_MySQL_PC_1.repository.ProductRepository;
 
 import java.util.List;
 
 public class ProductServiceImpl implements ProductService {
+
+    ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     @Override
     public Product addProduct(Product product) {
