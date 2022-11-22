95f883fdcb5bc3212e01a7107e5eb56725cedf15
Praveen Kumar <praveen170596@gmail.com>
2022-11-22 12:12:49 +0530

    created ProductServiceImpl class

--- src/main/java/com/niit/jdp/BEJ_C2_S1_REST_MySQL_PC_1/service/ProductService.java
+++ src/main/java/com/niit/jdp/BEJ_C2_S1_REST_MySQL_PC_1/service/ProductService.java
@@ -1,4 +1,14 @@
 package com.niit.jdp.BEJ_C2_S1_REST_MySQL_PC_1.service;
 
+import com.niit.jdp.BEJ_C2_S1_REST_MySQL_PC_1.domain.Product;
+
+import java.util.List;
+
 public interface ProductService {
+
+    Product addProduct(Product product);
+
+    List<Product> getAllProducts();
+
+    boolean deleteProductById(int productId);
 }
---
+++ src/main/java/com/niit/jdp/BEJ_C2_S1_REST_MySQL_PC_1/service/ProductServiceImpl.java
@@ -0,0 +1,11 @@
+/*
+ *Author Name:Praveen Kumar
+ *Date: 22-Nov-22
+ *Created With IntelliJ Idea Community Edition
+ */
+
+
+package com.niit.jdp.BEJ_C2_S1_REST_MySQL_PC_1.service;
+
+public class ProductServiceImpl {
+}
