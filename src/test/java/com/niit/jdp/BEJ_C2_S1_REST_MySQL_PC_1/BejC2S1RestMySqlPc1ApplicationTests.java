98374854983fef2336d8ccdb6359915cd76cebff
Praveen Kumar <praveen170596@gmail.com>
2022-11-22 11:06:31 +0530

    initial commit

---
+++ src/test/java/com/niit/jdp/BEJ_C2_S1_REST_MySQL_PC_1/BejC2S1RestMySqlPc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.jdp.BEJ_C2_S1_REST_MySQL_PC_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BejC2S1RestMySqlPc1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
