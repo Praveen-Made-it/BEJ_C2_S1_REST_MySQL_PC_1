3b796e3750688dbee21a1e085556ab8d8c5633b7
Praveen Kumar <praveen170596@gmail.com>
2022-11-22 11:09:38 +0530

    created attributes for the Product class

--- src/main/java/com/niit/jdp/BEJ_C2_S1_REST_MySQL_PC_1/domain/Product.java
+++ src/main/java/com/niit/jdp/BEJ_C2_S1_REST_MySQL_PC_1/domain/Product.java
@@ -8,4 +8,8 @@
 package com.niit.jdp.BEJ_C2_S1_REST_MySQL_PC_1.domain;
 
 public class Product {
+    private int productId;
+    private String productName;
+    private String productDescription;
+    private double amountInStock;
 }
