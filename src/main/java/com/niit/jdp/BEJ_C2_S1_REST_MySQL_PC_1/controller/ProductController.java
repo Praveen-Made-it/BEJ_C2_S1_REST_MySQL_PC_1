1288b71c2fc075c5d484d43c6790c57ce607a70a
Praveen Kumar <praveen170596@gmail.com>
2022-11-22 12:54:07 +0530

    created post mapping

--- src/main/java/com/niit/jdp/BEJ_C2_S1_REST_MySQL_PC_1/controller/ProductController.java
+++ src/main/java/com/niit/jdp/BEJ_C2_S1_REST_MySQL_PC_1/controller/ProductController.java
@@ -7,17 +7,27 @@
 
 package com.niit.jdp.BEJ_C2_S1_REST_MySQL_PC_1.controller;
 
+import com.niit.jdp.BEJ_C2_S1_REST_MySQL_PC_1.domain.Product;
 import com.niit.jdp.BEJ_C2_S1_REST_MySQL_PC_1.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/product/v1/")
+@RequestMapping("/productservice/v1/")
 public class ProductController {
 
     ProductService productService;
 
     public ProductController(ProductService productService) {
         this.productService = productService;
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestBody Product product) {
+        return new ResponseEntity<>(productService.addProduct(product), HttpStatus.CREATED);
     }
 }
