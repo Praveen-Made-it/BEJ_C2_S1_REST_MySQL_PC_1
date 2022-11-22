0fde2bf77d3d2701b5e32ed0b079ff9e9d8edafe
Praveen Kumar <praveen170596@gmail.com>
2022-11-22 12:54:59 +0530

    created get mapping for fetching the products

--- src/main/java/com/niit/jdp/BEJ_C2_S1_REST_MySQL_PC_1/controller/ProductController.java
+++ src/main/java/com/niit/jdp/BEJ_C2_S1_REST_MySQL_PC_1/controller/ProductController.java
@@ -11,10 +11,7 @@ import com.niit.jdp.BEJ_C2_S1_REST_MySQL_PC_1.domain.Product;
 import com.niit.jdp.BEJ_C2_S1_REST_MySQL_PC_1.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/productservice/v1/")
@@ -29,5 +26,10 @@ public class ProductController {
     @PostMapping("/product")
     public ResponseEntity<?> addProduct(@RequestBody Product product) {
         return new ResponseEntity<>(productService.addProduct(product), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<?> getAllProduct() {
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
     }
 }
