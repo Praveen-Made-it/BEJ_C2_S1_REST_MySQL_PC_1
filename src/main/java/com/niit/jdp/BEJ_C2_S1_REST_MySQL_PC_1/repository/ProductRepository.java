297cd536970404663994ddf5cd3d4f8cbc429588
Praveen Kumar <praveen170596@gmail.com>
2022-11-22 12:04:24 +0530

    created ProductService interface

--- src/main/java/com/niit/jdp/BEJ_C2_S1_REST_MySQL_PC_1/repository/ProductRepository.java
+++ src/main/java/com/niit/jdp/BEJ_C2_S1_REST_MySQL_PC_1/repository/ProductRepository.java
@@ -1,5 +1,10 @@
 package com.niit.jdp.BEJ_C2_S1_REST_MySQL_PC_1.repository;
 
-public interface ProductRepository {
+import com.niit.jdp.BEJ_C2_S1_REST_MySQL_PC_1.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 }
---
+++ src/main/java/com/niit/jdp/BEJ_C2_S1_REST_MySQL_PC_1/service/ProductService.java
@@ -0,0 +1,4 @@
+package com.niit.jdp.BEJ_C2_S1_REST_MySQL_PC_1.service;
+
+public interface ProductService {
+}
